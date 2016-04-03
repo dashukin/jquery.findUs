0b12f4d1355ed0210d18921717df91d6e7a3bfd7
dashukin <dashukin@gmail.com>
2016-04-03 23:30:53 +0300

    Update plugin version number.

--- src/jquery.findUs.js
+++ src/jquery.findUs.js
@@ -4,7 +4,7 @@
  * @license MIT License; http://www.opensource.org/licenses/mit-license.php
  * @url   https://github.com/dashukin/jquery.findUs
  * @author  Vasili Molakhau <dashukin@gmail.com>
- * @version 0.1.0
+ * @version 0.2.0
  */
 ;(function ($) {
 
